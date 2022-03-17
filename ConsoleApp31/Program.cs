b651687c9c83b3fa564d02ee178ffcad39f48f57
tomekzpolski1 <tomasz.rozynski92@gmail.com>
2022-03-17 09:55:25 +0100

    Add project files.

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pierwszy sposob
+            int[] array = new int[5]; //deklaracja ablicy 5 "szuflad"
+            array[0] = 1;
+            array[1] = 2;
+            array[2] = 3;
+            array[3] = 4;
+            //Console.WriteLine(array[0]);
+            Console.WriteLine("Lenght : {0}", array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            //drugi sposob
+            char[] array2 = { 'H', 'e', 'l', 'l', 'o' };
+            foreach (char item in array2) //jaki typ zmiennych do iteracji (char), item - tak ma sie nazywac, array2 - w czym mamy iterować
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
